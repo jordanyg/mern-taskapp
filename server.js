b2bf3a8e6616ad2c9e6a914e4419318aa4bf3374
Yordanos Getachew <jordangetachew49@gmail.com>
2026-05-02 17:20:02 +0300

    feat: add model and DB config

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
 
+
+
+dotenv.config()
+connectDB()
 
 const app = express()
 const port = 8000
