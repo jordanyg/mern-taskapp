357d3d98c474632489d396b4a56f2e089edc5a81
Yordanos Getachew <jordangetachew49@gmail.com>
2026-05-03 15:45:45 +0300

    feat: work on controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,27 @@
-const createUser = (req ,res)=>{
-    res.status(200).json({message : 'route working'})
+import User from "../models/userModel"
+import asyncHandler from 'express-async-handler'
+
+const createUser = async(req ,res)=>{
+    const [name , email , password] = req.body
+    if(!name || !email || !password){
+        res.status(400).json({message : 'all fields required'})
+    }
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        console.log('user already exists')
+    }
+    const user = await User.create({name , email , password})
+
+    if(user){
+        res.status(201)
+        res.status(201).json({
+            id : user._id,
+            name : user.name,
+            email :user.email
+        })
+    }
+    
 }
 const updateUser = (req, res)=>{
     res.status(200).json({message : 'user updated'})
