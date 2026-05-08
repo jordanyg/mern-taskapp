b08fe952ea43d878dee66e806cd5eb84401cde5c
Yordanos Getachew <jordangetachew49@gmail.com>
2026-05-08 15:25:50 +0300

    feat: protected routes that need authentication

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import asyncHandler from "express-async-handler"
+import  jwt  from "jsonwebtoken"
+import User from "../models/userModel"
+
+const protect = asyncHandler(async (req,res,next)=>{
+    let token
+    token = req.cookies.jwt
+    if(token){
+        try {
+            const decoded =  jwt.verify(token , process.env.JWT_SECRET)
+            res.user = await User.findById(decoded.userId).select('-password')
+            next()
+        } catch (error) {
+            res.status(400)
+            throw new Error('unauthorized, invalid token')
+        }    
+    }
+    if(!token){
+        res.status(400)
+        throw new Error('unauthorized, no token')
+    }
+})
+
+export default protect
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { createUser  ,updateUser ,loginUser , deleteUser} from "../controllers/userController.js";
 import express from 'express'
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router()
 
 router.post('/register' ,createUser)
-router.put('/updateUser', updateUser )
-router.delete('/profile' , deleteUser)
+router.put('/updateUser',protect, updateUser )
+router.delete('/profile' ,protect, deleteUser)
 router.post('/login' , loginUser)
 
 
